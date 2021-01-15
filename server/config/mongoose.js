dec0aa68ea486eb63bace05e545eae09480e9a5a
Juan <pront23@gmail.com>
2021-01-15 17:01:46 +0900

    updated client connection options

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,15 +1,17 @@
-const fs = require('fs')
-const path =require("path");
+const fs = require('fs');
+const path = require('path');
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/DUber');
-
+mongoose.connect('mongodb://localhost/DUber', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 var models_path = path.join(__dirname, './../models');
 // read all of the files in the models_path and require (run) each of the javascript files
-fs.readdirSync(models_path).forEach(function(file) {
-  if(file.indexOf('.js') >= 0) {
+fs.readdirSync(models_path).forEach(function (file) {
+  if (file.indexOf('.js') >= 0) {
     // require the file (this runs the model file which registers the schema)
     require(models_path + '/' + file);
-   }
-})
+  }
+});
