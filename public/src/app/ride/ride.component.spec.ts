686738b8c4a37defca0b258b6fc88eb85c6b2d48
Juan <pront23@gmail.com>
2021-01-15 17:01:24 +0900

    updated angular

--- public/src/app/ride/ride.component.spec.ts
+++ public/src/app/ride/ride.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RideComponent } from './ride.component';
 
@@ -6,7 +6,7 @@ describe('RideComponent', () => {
   let component: RideComponent;
   let fixture: ComponentFixture<RideComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RideComponent ]
     })
